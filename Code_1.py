a15c66f6c7ca79bbc01a01838cc20803883b3460
Wenwu_Home <1465360495@qq.com>
2020-08-21 22:17:53 +0800

    First day of learning python

--- Code_1.py
+++ Code_1.py
@@ -1,1 +1,30 @@
-print("hello, world!")
+# -*-  coding: utf-8 -*-
+
+age = 20
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenage')
+else:
+    print('kid')
+
+names = ('Lisa', 'Mary', 'Henry')
+for x in names:
+    print(x)
+
+
+x = [1, 2, 3]
+y = (1, 2, 3)
+
+def fun(*num):
+    sum = 0
+    for x in num:
+        sum = sum + x
+    return sum
+
+sum = fun(1, 2, 3)
+print(sum)
+
+s = [1, 2, 3]
+sum = fun(*s)
+print(sum)
