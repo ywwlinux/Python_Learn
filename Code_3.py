1f98779c647abc2217e2a6ef77633f2c007c445d
Wenwu_Home <1465360495@qq.com>
2020-08-28 22:00:38 +0800

    4th day of learning python

--- Code_3.py
+++ Code_3.py
@@ -5,7 +5,13 @@
 __author__ = "Wenwu Yang"
 
 class Student(object):
-    pass
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    
+    def print_score(self):
+        print('%s: %s'%(self.name, self.score))
 
-bart = Student()
-print(bart)
+bart = Student('FangFang', 80)
+bart.print_score()
+
