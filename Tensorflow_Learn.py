4d58a8c5938b5f810019d445ba9149ec48cedd75
Wenwu_Home <1465360495@qq.com>
2020-09-08 11:06:39 +0800

    update of the usage of tf.data

--- Tensorflow_Learn.py
+++ Tensorflow_Learn.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 
 import numpy as np
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 ' Code for learning tensorflow '
 
@@ -20,18 +21,53 @@ def read_birth_life_data(filename):
 
 	return data, n_sample
 
+
 # Step 1: read in the data
 data, n_sample = read_birth_life_data(DATA_FILE)
 
-# Step 2: assemble the graph for linear regression neural net
-# Y = wX + b
-X = tf.placeholder(tf.float32, name = 'X')
-Y = tf.placeholder(tf.float32, name = 'Y')
+# Use the huber loss
+def huber_loss(labels, predictions, delta=14.0):
+    residual = tf.abs(labels - predictions)
+    def f1(): return 0.5 * tf.square(residual)
+    def f2(): return delta * residual - 0.5 * tf.square(delta)
+    return tf.cond(residual < delta, f1, f2)
 
-w = tf.get_variable('weights', initializer=tf.constant(0.0))
-b = tf.get_variable('bias', initializer=tf.constant(.0))
+def use_placeholder_for_data():
+	# Step 2: assemble the graph for linear regression neural net
+	# Y = wX + b
+	X = tf.placeholder(tf.float32, name = 'X')
+	Y = tf.placeholder(tf.float32, name = 'Y')
 
-Y_predicted = w*X + b
+	w = tf.get_variable('weights', initializer=tf.constant(0.0))
+	b = tf.get_variable('bias', initializer=tf.constant(.0))
+
+	Y_predicted = w*X + b
+
+	loss2 = huber_loss(Y, Y_predicted) 
+	optimizer2 = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(loss2)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for i in range(100): # 100 epochs
+			total_loss = 0
+			for x, y in data:
+				_, l = sess.run([optimizer2, loss2], feed_dict={X:x, Y:y})
+				total_loss += l
+			
+			print('Epoch {0}: {1}'.format(i, total_loss/n_sample))
+
+		w_out2, b_out2 = sess.run([w, b])
+
+	# Evaluate the results
+	print('w, b: (%f, %f)\n'%(w_out2, b_out2))
+
+	# plot the results
+	plt.plot( data[:,0], data[:, 1], 'bo', label='Real data' )
+	# plt.plot( data[:,0], data[:,0]*w_out+b_out, 'r', label='Predicted data:square_loss'  )
+	plt.plot( data[:,0], data[:,0]*w_out2+b_out2, 'g', label='Predicted data:huber_loss'  )
+	plt.legend()
+	plt.show()	
 
 # # Y = wX^2 + uX + b
 # X = tf.placeholder(tf.float32, name = 'X')
@@ -43,58 +79,70 @@ Y_predicted = w*X + b
 
 # Y_predicted = w*X*X + u*X + b
 
-loss = tf.square(Y - Y_predicted, name = 'loss')
+# loss = tf.square(Y - Y_predicted, name = 'loss')
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(loss)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(loss)
 
-# Step3: run the session
-writer = tf.summary.FileWriter('./graphs', tf.get_default_graph()) #tensorboard --logdir="./graphs" --port 6006; http://localhost:6006/
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
+# # Step3: run the session
+# writer = tf.summary.FileWriter('./graphs', tf.get_default_graph()) #tensorboard --logdir="./graphs" --port 6006; http://localhost:6006/
+# with tf.Session() as sess:
+# 	sess.run(tf.global_variables_initializer())
 
-	for i in range(100): # 100 epochs
-		total_loss = 0
-		for x, y in data:
-			_, l = sess.run([optimizer, loss], feed_dict={X:x, Y:y})
-			total_loss += l
+# 	for i in range(100): # 100 epochs
+# 		total_loss = 0
+# 		for x, y in data:
+# 			_, l = sess.run([optimizer, loss], feed_dict={X:x, Y:y})
+# 			total_loss += l
 		
-		print('Epoch {0}: {1}'.format(i, total_loss/n_sample))
+# 		print('Epoch {0}: {1}'.format(i, total_loss/n_sample))
 	  
-	writer.close()
+# 	writer.close()
 	
-	w_out, b_out = sess.run([w, b])
+# 	w_out, b_out = sess.run([w, b])
 
-# Evaluate the results
-print('w, b: (%f, %f)\n'%(w_out, b_out))
+# # Evaluate the results
+# print('w, b: (%f, %f)\n'%(w_out, b_out))
+
+def use_tfdata_for_data():
+	dataset = tf.data.Dataset.from_tensor_slices((data[:,0], data[:,1]))
+	print(dataset.output_types)
+	print(dataset.output_shapes)
+
+	# iterator = dataset.make_one_shot_iterator()
+	iterator = dataset.make_initializable_iterator()
+	X, Y = iterator.get_next()
+
+	w = tf.get_variable('weights', initializer=tf.constant(0.0))
+	b = tf.get_variable('bias', initializer=tf.constant(.0))
+
+	Y_predicted = w*X + b
+
+	loss2 = huber_loss(Y, Y_predicted) 
+	optimizer2 = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(loss2)
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for i in range(100):
+			sess.run(iterator.initializer)
+			total_loss = .0
+			try:
+				while True:
+					_, l = sess.run([optimizer2, loss2])
+					total_loss += l
+			except tf.errors.OutOfRangeError:
+				pass
+
+			print('Epoch {0}: {1}'.format(i, total_loss/n_sample))
+
+		w_out2, b_out2 = sess.run([w, b])
+
+	# Evaluate the results
+	print('w, b: (%f, %f)\n'%(w_out2, b_out2))
 
 
-# Use the huber loss
-def huber_loss(labels, predictions, delta=14.0):
-    residual = tf.abs(labels - predictions)
-    def f1(): return 0.5 * tf.square(residual)
-    def f2(): return delta * residual - 0.5 * tf.square(delta)
-    return tf.cond(residual < delta, f1, f2)
+if __name__ == '__main__':
+	# use_placeholder_for_data()
+	use_tfdata_for_data()
 
-loss2 = huber_loss(Y, Y_predicted) 
-optimizer2 = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(loss2)
-
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-
-	for i in range(100): # 100 epochs
-		total_loss = 0
-		for x, y in data:
-			_, l = sess.run([optimizer2, loss2], feed_dict={X:x, Y:y})
-			total_loss += l
-		
-		print('Epoch {0}: {1}'.format(i, total_loss/n_sample))
-
-	w_out2, b_out2 = sess.run([w, b])
-
-# plot the results
-plt.plot( data[:,0], data[:, 1], 'bo', label='Real data' )
-plt.plot( data[:,0], data[:,0]*w_out+b_out, 'r', label='Predicted data:square_loss'  )
-plt.plot( data[:,0], data[:,0]*w_out2+b_out2, 'g', label='Predicted data:huber_loss'  )
-plt.legend()
-plt.show()	
 
